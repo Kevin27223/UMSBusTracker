6b7d883dffc8a0a574bb9ecfba26e979bef27c3e
hocheeseng93 <hocheeseng1993@gmail.com>
2017-05-20 23:47:34 +0800

    Completed RouteActivity

---
+++ app/src/main/java/com/example/cheeseng/umsbustracker/ExpandableListAdapter.java
@@ -0,0 +1,93 @@
+package com.example.cheeseng.umsbustracker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Chee Seng on 15-May-17.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Activity context;
+    private Map<String, List<String>> groupCollections;
+    private List<String> childList;
+
+    public ExpandableListAdapter(Activity context, List<String> childList,
+                                 Map<String, List<String>> groupCollections) {
+        this.context = context;
+        this.groupCollections = groupCollections;
+        this.childList = childList;
+    }
+
+    public Object getChild(int groupPosition, int childPosition) {
+        return groupCollections.get(childList.get(groupPosition)).get(childPosition);
+    }
+
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+
+    public View getChildView(final int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String laptop = (String) getChild(groupPosition, childPosition);
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.route_child, null);
+        }
+
+        TextView item = (TextView) convertView.findViewById(R.id.route);
+
+        item.setText(laptop);
+        return convertView;
+    }
+
+    public int getChildrenCount(int groupPosition) {
+        return groupCollections.get(childList.get(groupPosition)).size();
+    }
+
+    public Object getGroup(int groupPosition) {
+        return childList.get(groupPosition);
+    }
+
+    public int getGroupCount() {
+        return childList.size();
+    }
+
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String laptopName = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.route_group,
+                    null);
+        }
+        TextView item = (TextView) convertView.findViewById(R.id.route);
+        item.setTypeface(null, Typeface.BOLD);
+        item.setText(laptopName);
+        return convertView;
+    }
+
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
--- app/src/main/java/com/example/cheeseng/umsbustracker/RouteActivity.java
+++ app/src/main/java/com/example/cheeseng/umsbustracker/RouteActivity.java
@@ -4,10 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chee Seng on 22-Nov-16.
@@ -15,16 +22,32 @@ import android.widget.Toast;
 
 public class RouteActivity extends AppCompatActivity {
 
+    List<String> groupList;
+    List<String> childList;
+    Map<String, List<String>> routeCollection;
+    ExpandableListView expListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
+
+        createGroupList();
+
+        createCollection();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.route_toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        expListView = (ExpandableListView) findViewById(R.id.route_list);
+        final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(
+                this, groupList, routeCollection);
+        expListView.setAdapter(expListAdapter);
+
+        setGroupIndicatorToRight();
     }
 
     @Override
@@ -36,5 +59,66 @@ public class RouteActivity extends AppCompatActivity {
                 startActivity(homeIntent);
         }
         return (super.onOptionsItemSelected(menuItem));
+    }
+
+    private void createGroupList() {
+        groupList = new ArrayList<String>();
+        groupList.add("Campus");
+        groupList.add("Usia");
+        groupList.add("Kingfisher");
+        groupList.add("Sri Angkasa");
+    }
+
+    private void createCollection() {
+        String[] campusModels = { "DKP Lama - K.Resital", "K.Resital - DKP Baru",
+                "DKP Baru - K.Resital", "K.Resital - FKJ", "FKJ - PPIB", "PPIB - DKP Lama"};
+        String[] usiaModels = { "USIA - FSSA", "FSSA - PPIB", "PPIB - DKP Lama",
+                "DKP Lama - DKP Baru", "DKP Baru - DKP Lama", "DKP Lama - PPIB", "PPIB - FKJ",
+                "FKJ - USIA"};
+        String[] kingfisherModels = { "KF - FSSA", "FSSA - PPIB", "PPIB - DKP Lama",
+                "DKP Lama - DKP Baru", "DKP Baru - DKP Lama", "DKP Lama - PPIB", "PPIB - FKJ",
+                "FKJ - KF" };
+        String[] angkasaModels = { "Angkasa - FSSA", "FSSA - PPIB", "PPIB - DKP Lama",
+                "DKP Lama - DKP Baru", "DKP Baru - DKP Lama", "DKP Lama - PPIB", "PPIB - FKJ",
+                "FKJ - Angkasa" };
+
+        routeCollection = new LinkedHashMap<String, List<String>>();
+
+        for (String route : groupList) {
+            if (route.equals("Campus")) {
+                loadChild(campusModels);
+            } else if (route.equals("Usia"))
+                loadChild(usiaModels);
+            else if (route.equals("Kingfisher"))
+                loadChild(kingfisherModels);
+            else
+                loadChild(angkasaModels);
+
+            routeCollection.put(route, childList);
+        }
+    }
+
+    private void loadChild(String[] laptopModels) {
+        childList = new ArrayList<String>();
+        for (String model : laptopModels)
+            childList.add(model);
+    }
+
+    private void setGroupIndicatorToRight() {
+        /* Get the screen width */
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+
+        expListView.setIndicatorBounds(width - getDipsFromPixel(35), width
+                - getDipsFromPixel(5));
+    }
+
+    // Convert pixel to dip
+    public int getDipsFromPixel(float pixels) {
+        // Get the screen's density scale
+        final float scale = getResources().getDisplayMetrics().density;
+        // Convert the dps to pixels, based on density scale
+        return (int) (pixels * scale + 0.5f);
     }
 }
